853fb1954087280fc7f357ca28dadc5c8ece5d7b
zhouxinseeu <zhouxin@singleronbio.com>
2021-02-24 17:07:31 +0800

    add Bio.Seq for translate

--- BCR_filter.py
+++ BCR_filter.py
@@ -1,29 +1,6 @@
 import pandas as pd
 import argparse
-
-
-gencode = {
-      'ATA': 'I', 'ATC': 'I', 'ATT': 'I', 'ATG': 'M',
-      'ACA': 'T', 'ACC': 'T', 'ACG': 'T', 'ACT': 'T',
-      'AAC': 'N', 'AAT': 'N', 'AAA': 'K', 'AAG': 'K',
-      'AGC': 'S', 'AGT': 'S', 'AGA': 'R', 'AGG': 'R',
-      'CTA': 'L', 'CTC': 'L', 'CTG': 'L', 'CTT': 'L',
-      'CCA': 'P', 'CCC': 'P', 'CCG': 'P', 'CCT': 'P',
-      'CAC': 'H', 'CAT': 'H', 'CAA': 'Q', 'CAG': 'Q',
-      'CGA': 'R', 'CGC': 'R', 'CGG': 'R', 'CGT': 'R',
-      'GTA': 'V', 'GTC': 'V', 'GTG': 'V', 'GTT': 'V',
-      'GCA': 'A', 'GCC': 'A', 'GCG': 'A', 'GCT': 'A',
-      'GAC': 'D', 'GAT': 'D', 'GAA': 'E', 'GAG': 'E',
-      'GGA': 'G', 'GGC': 'G', 'GGG': 'G', 'GGT': 'G',
-      'TCA': 'S', 'TCC': 'S', 'TCG': 'S', 'TCT': 'S',
-      'TTC': 'F', 'TTT': 'F', 'TTA': 'L', 'TTG': 'L',
-      'TAC': 'Y', 'TAT': 'Y', 'TAA': '_', 'TAG': '_',
-      'TGC': 'C', 'TGT': 'C', 'TGA': '_', 'TGG': 'W'}
-
-
-def translate_frameshifted(sequence):
-    translate = ''.join([gencode.get(sequence[3 * i:3 * i + 3], 'X') for i in range(len(sequence) // 3)])
-    return translate
+from Bio.Seq import Seq
 
 
 def filter_by_tpm(bracer_outdir):
@@ -75,7 +52,8 @@ def filter_by_tpm(bracer_outdir):
         cdr3 = list(tep['CDR3'])
         aaseq = []
         for seq in cdr3:
-            seq = translate_frameshifted(seq)
+            seq = Seq(seq)
+            seq = seq.translate()
             aaseq.append(seq)
         string = 'IG{}:{}\nIG{}:{}'.format(tep_loci[0], aaseq[0], tep_loci[1], aaseq[1])
         aaseqs.append(string)
